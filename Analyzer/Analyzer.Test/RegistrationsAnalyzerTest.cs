db8c8422a980797a43c27314f36a9b12324f12b3
Martin Strecker <martin.strecker@sonarsource.com>
2023-11-10 12:53:26 +0100

    Add project files.

---
+++ Analyzer/Analyzer.Test/RegistrationsAnalyzerTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Verifyer = Microsoft.CodeAnalysis.CSharp.Testing.MSTest.AnalyzerVerifier<Analyzer.RegistrationsAnalyzer>;
+namespace Analyzer.Test
+{
+    [TestClass]
+    public class RegistrationsAnalyzerTest
+    {
+        [TestMethod]
+        public async Task DoesNotFail()
+        {
+            await Verifyer.VerifyAnalyzerAsync("");
+        }
+
+        [TestMethod] // https://github.com/dotnet/roslyn-sdk/blob/main/src/Microsoft.CodeAnalysis.Testing/README.md
+        public async Task StringBuilderExtension_OtherSymbolReturned()
+        {
+            await Verifyer.VerifyAnalyzerAsync("""
+            using System.Text;
+
+            public static class StringBuilderExtensions
+            {
+                public static StringBuilder MyExtension(this StringBuilder sb)
+                {
+                    var result = new StringBuilder();
+                    [|return result;|]
+                }
+            }
+            """);
+        }
+
+        [TestMethod]
+        public async Task StringBuilderExtension_AssignedSymbolReturned()
+        {
+            await Verifyer.VerifyAnalyzerAsync("""
+            using System.Text;
+
+            public static class StringBuilderExtensions
+            {
+                public static StringBuilder MyExtension(this StringBuilder sb)
+                {
+                    StringBuilder result;
+                    result = sb;
+                    return result;
+                }
+            }
+            """);
+        }
+    }
+}
---
+++ Analyzer/Analyzer/RegistrationsAnalyzer.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Concurrent;
+using System.Xml;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq;
+
+namespace Analyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class RegistrationsAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "MS002";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId,
+            "The string builder passed in the this parameter should be returned",
+            "The returned value is not the parameter passed via the this parameter",
+            category: "Naming", DiagnosticSeverity.Warning, isEnabledByDefault: true, description: "Description.");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterCompilationStartAction(CompilationStart);
+        }
+
+        private static void CompilationStart(CompilationStartAnalysisContext context)
+        {
+            if (context.Compilation.GetTypeByMetadataName("System.Text.StringBuilder") is INamedTypeSymbol stringBuilderTypeSymbol)
+            {
+                context.RegisterSymbolStartAction((SymbolStartAnalysisContext symbolStartContext) =>
+                {
+                    if (symbolStartContext.Symbol.IsStatic)
+                    {
+                        symbolStartContext.RegisterCodeBlockStartAction((CodeBlockStartAnalysisContext<SyntaxKind> codeBlockContext) =>
+                        {
+                            if (codeBlockContext.OwningSymbol is IMethodSymbol { IsExtensionMethod: true, Parameters.Length: >= 1 } methodSymbol
+                                && methodSymbol.Parameters[0] is { } thisParameter
+                                && SymbolEqualityComparer.Default.Equals(thisParameter.Type, stringBuilderTypeSymbol)
+                                && SymbolEqualityComparer.Default.Equals(methodSymbol.ReturnType, stringBuilderTypeSymbol))
+                            {
+                                var thisStringBuilderReferences = new ConcurrentDictionary<ISymbol, object>(SymbolEqualityComparer.Default);
+                                var stringBuilderReturns = new ConcurrentDictionary<ISymbol, SyntaxNode>(SymbolEqualityComparer.Default);
+                                thisStringBuilderReferences.TryAdd(thisParameter, null);
+
+                                codeBlockContext.RegisterSyntaxNodeAction((SyntaxNodeAnalysisContext assignmentContext) =>
+                                {
+                                    var assignment = (AssignmentExpressionSyntax)assignmentContext.Node;
+                                    if (assignmentContext.SemanticModel.GetSymbolInfo(assignment.Right).Symbol is { } assignedSymbol
+                                        && SymbolEqualityComparer.Default.Equals(assignedSymbol, thisParameter)
+                                        && assignmentContext.SemanticModel.GetSymbolInfo(assignment.Left).Symbol is { } assignedToSymbol)
+                                    {
+                                        thisStringBuilderReferences.TryAdd(assignedToSymbol, null);
+                                    }
+                                }, SyntaxKind.SimpleAssignmentExpression);
+
+                                codeBlockContext.RegisterSyntaxNodeAction((SyntaxNodeAnalysisContext returnContext) =>
+                                {
+                                    var returnStatement = (ReturnStatementSyntax)returnContext.Node;
+                                    if (returnContext.SemanticModel.GetSymbolInfo(returnStatement.Expression).Symbol is { } returnSymbol)
+                                    {
+                                        stringBuilderReturns.TryAdd(returnSymbol, returnStatement);
+                                    }
+                                }, SyntaxKind.ReturnStatement);
+
+                                codeBlockContext.RegisterCodeBlockEndAction((CodeBlockAnalysisContext endContext) =>
+                                {
+                                    foreach (var stringBuilderReturn in stringBuilderReturns.Where(kvp => !thisStringBuilderReferences.ContainsKey(kvp.Key)))
+                                    {
+                                        endContext.ReportDiagnostic(Diagnostic.Create(Rule, stringBuilderReturn.Value.GetLocation()));
+                                    }
+                                });
+                            }
+                        });
+                    }
+                }, SymbolKind.NamedType);
+            }
+        }
+    }
+}
+
